6046bc2ce18b8074816c9b72c3e93f8f7e940399
keemeesuu <kimhogy@gmail.com>
2021-05-18 19:24:25 +0900

    ^^

--- SwiftUI_Webview_tutorial/ContentView.swift
+++ SwiftUI_Webview_tutorial/ContentView.swift
@@ -9,8 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        MyWebview(urlToLoad: "https://www.naver.com")
+
+        NavigationView{
+            HStack{
+                NavigationLink(destination: MyWebview(urlToLoad: "http://www.naver.com").edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                ){
+                    Text("네이버")
+                        .font(.system(size: 20))
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .padding(20)
+                        .background(Color.green)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(20)
+                }
+                NavigationLink(destination: MyWebview(urlToLoad: "http://www.daum.net").edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)){
+                    Text("다음")
+                        .font(.system(size: 20))
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .padding(20)
+                        .background(Color.orange)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(20)
+                }
+                NavigationLink(destination: MyWebview(urlToLoad: "http://www.google.com").edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)){
+                    Text("구글")
+                        .font(.system(size: 20))
+                        .fontWeight(.bold)
+                        .padding(20)
+                        .background(Color.blue)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(20)
+                }
+            }
+        }
+        
     }
 }
 
