7a9a04ddb602e112a6ed9698431693732e02f4b0
keemeesuu <kimhogy@gmail.com>
2021-05-18 18:34:12 +0900

    Initial Commit

---
+++ SwiftUI_Webview_tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Webview_tutorial
+//
+//  Created by hogy on 2021/05/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Webview_tutorial/SwiftUI_Webview_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Webview_tutorialApp.swift
+//  SwiftUI_Webview_tutorial
+//
+//  Created by hogy on 2021/05/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Webview_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
